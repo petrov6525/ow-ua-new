48ba94a51f00b7c39f3ec5af4041a3f5dbe2fe7b
AlexDaryncev <petrov6525@gmail.com>
2024-02-03 23:17:44 +0300

    commit to master

--- screens/video_page/VideoPage.js
+++ screens/video_page/VideoPage.js
@@ -3,23 +3,27 @@ import {Text, View, StyleSheet, SafeAreaView} from "react-native";
 import {fontStyles} from "../../styles/font";
 import { useRoute } from '@react-navigation/native';
 import {useRef} from "react";
+import React from "react";
 
 export const VideoPage = () => {
     const route = useRoute();
-    const { video } = route.params;
+    const { videoParams } = route.params;
+    const videoPlayer = React.useRef(null);
+    const [status, setStatus] = React.useState({});
 
 
     return(
         <MainLayout>
             <SafeAreaView>
-                {/*<VideoPlayer
-                    video={{ uri: require('../../assets/video/video_1.mp4') }}
-                    videoWidth={1600}
-                    videoHeight={900}
-                    thumbnail={{ uri: require('../../assets/logo.png') }}
-                    autoplay={false}
-                />*/}
+
             </SafeAreaView>
         </MainLayout>
     )
 }
+
+const styles = StyleSheet.create({
+    video: {
+        flex: 1,
+        alignSelf: 'stretch'
+    }
+})
